f4fc704e8a56cbccb4de46acdaba693d45008a25
Hyemin Lee <hyemin.lee@voyagerx.com>
2022-09-22 21:03:07 +0900

    print input text in console log

--- src/InputData.js
+++ src/InputData.js
@@ -1,14 +1,63 @@
 import React, { useState } from "react";
-import './InputData.css';
+import "./InputData.css";
+function InputText(props) {
+  return (
+    <article>
+      <div className="box-container">
+        <form
+          onSubmit={(event) => {
+            event.preventDefault();
 
-function InputData(){
-    return (
-        <div>
-            내용 <input placeholder="내용"/> &ensp;
-            작성자 <input placeholder="작성자"/> &ensp;
-            <button id="submitButton">작성하기</button>
-        </div>
-    );
+            const content = event.target.content.value;
+            const writer = event.target.writer.value;
+            console.log(`in onSubmit function`);
+            props.onCreate(content, writer);
+          }}
+        >
+          <p>
+            <textarea name="content" placeholder="메모 내용" />
+          </p>
+          <p>
+            <input type="text" name="writer" placeholder="작성자" />
+          </p>
+          <p>
+            <input type="submit" value="작성"></input>
+          </p>
+        </form>
+      </div>
+    </article>
+  );
+}
+
+function InputData() {
+  const [memoId, setMemoId] = useState(null);
+  const [nextMemoId, setNextMemoID] = useState(1);
+  const [memoContainer, setMemoContainer] = useState([
+    { id: 1, content: "sample memo content", writer: "hyemin lee" },
+  ]);
+
+  return (
+    <div>
+      <InputText
+        onCreate={(_content, _writer) => {
+          const newMemo = {
+            id: nextMemoId,
+            content: _content,
+            writer: _writer,
+          };
+          const newMemoContainer = [...memoContainer];
+          newMemoContainer.push(newMemo);
+        
+          setMemoContainer(newMemoContainer);
+          setMemoId(nextMemoId);
+          setNextMemoID(nextMemoId + 1);
+
+          // console.log(`memo container: ${memoContainer}`);
+          // console.log(`content in memo: ${JSON.stringify(memoContainer)}`);
+        }}
+      ></InputText>
+    </div>
+  );
 }
 
 export default InputData;
---
+++ src/LocalStorage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function LocalStorage(){
+
+}
+
+export default LocalStorage;
