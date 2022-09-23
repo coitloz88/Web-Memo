fa1af3b4a38d886fb58f1d9a1f1d0ac2e7118569
Hyemin Lee <hyemin.lee@voyagerx.com>
2022-09-23 17:50:52 +0900

    add skeleton of local storage

--- src/storage/LocalStorage.js
+++ src/storage/LocalStorage.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
 function LocalStorage(){
+    const [id, setId] = useState(0);
+    const [inputContent, setContent] = useState("");
+    const [inputWriter, setWriter] = useState("");
 
+    const saveData = () => {
+        const memoItem = { content: inputContent, writer: inputWriter};
+        window.localStorage.setItem("memo", JSON.stringify(memoItem));
+    };
 }
 
 export default LocalStorage;
